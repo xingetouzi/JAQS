37db9d5caa17b4be4e2d8afbba9fb395f883a97b
Rich <tianrq10@163.com>
2018-01-11 21:31:05 +0800

    提高多个信号一套配置下的计算效率-SignalCreator

--- jaqs/research/__init__.py
+++ jaqs/research/__init__.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
-from .signaldigger import process,multi_factor,Optimizer,SignalDigger
+from .signaldigger import process,multi_factor,Optimizer,SignalDigger,SignalCreator
 from .signaldigger.analysis import analysis
 
 
-__all__ = ['SignalDigger',"Optimizer","analysis"]
+__all__ = ['SignalDigger',"Optimizer","analysis","SignalCreator"]
--- jaqs/research/signaldigger/__init__.py
+++ jaqs/research/signaldigger/__init__.py
@@ -2,7 +2,8 @@
 
 from .digger import SignalDigger
 from .optimizer import Optimizer
+from .signal_creator import SignalCreator
 from . import process,multi_factor,analysis
 
 
-__all__ = ['SignalDigger',"Optimizer"]
+__all__ = ['SignalDigger',"Optimizer","SignalCreator"]
